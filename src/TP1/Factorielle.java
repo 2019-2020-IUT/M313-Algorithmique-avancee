ef2317ac612d0e144a4f850ede3faa0fb40dc01c
JunkJumper <jose.srifi@gmail.com>
2020-09-02 14:12:35 +0200

    exo1 ok

--- src/TP1/Factorielle.java
+++ src/TP1/Factorielle.java
@@ -2,4 +2,24 @@ package TP1;
 
 public class Factorielle {
 
+	public static int fact(int n) {
+		if(n < 0) {
+			return (int)Math.pow(-1, Math.abs(n)) * fact(Math.abs(n));
+		} else if (n == 0){
+			return 1;
+		} else {
+			return n * fact(n-1);
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println(fact(-1));
+		System.out.println(fact(-3));
+		System.out.println(fact(-4));
+		System.out.println(fact(-5));
+		System.out.println(fact(0));
+	}
 }
+
+
